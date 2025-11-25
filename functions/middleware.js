105bb9105ed31e3b574fc3310555e24ac79a8f57
you3333ef <you3333ef@example.com>
2025-11-25 12:27:51 +0300

    Fix middleware.js - make handler async to support await next()

--- functions/middleware.js
+++ functions/middleware.js
@@ -1,7 +1,9 @@
-export const onRequest = [{ 
-  request ({ request, next }) {
+export const onRequest = [
+  async ({ request, next }) => {
     const response = await next(request);
     response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     return response;
   }
-}];
+];
